015c98b1a0e077151104362b3cbea859daceb987
Marie P <map40j@gmail.com>
2023-02-04 21:32:18 -0800

    Started working on the tag Routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -26,7 +26,7 @@ router.get('/:id', (req, res) => {
     Product.findOne({
       where: {
         id: req.params.id,
-      }
+    },
      include: [
        Category,
        {
@@ -55,7 +55,7 @@ router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
+      if (req.body.tagIds && req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
@@ -84,6 +84,7 @@ router.put('/:id', (req, res) => {
   })
     .then((product) => {
       // find all associated tags from ProductTag
+  if (req.body.tagIds && req.body.tagIds.length) 
       return ProductTag.findAll({ where: { product_id: req.params.id } });
     })
     .then((productTags) => {
@@ -118,6 +119,19 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((products) => {
+    console.log(products);
+    res.json(products);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err);
+  });
 });
 
 module.exports = router;
