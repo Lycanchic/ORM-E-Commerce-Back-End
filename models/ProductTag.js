793f015a2b5724c9bca1924b868ec4e46f821093
Marie P <map40j@gmail.com>
2023-02-11 12:51:53 -0800

    cont' working on the tables

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,6 +1,8 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const Product = require('./Product');
+const Tag = require('./Tag');
 
 class ProductTag extends Model {}
 
@@ -12,6 +14,14 @@ ProductTag.init(
       autoIncrement: true,
       primaryKey: true,
     },
+    product_id: {
+     key: "id",
+     references: Product,
+    },
+    tag_id: {
+      key: "id",
+      references: Tag,
+    }
   },
   {
     sequelize,
