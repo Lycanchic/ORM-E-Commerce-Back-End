25749e3f035424806fd1fcd369d24b01775ca704
Marie P <map40j@gmail.com>
2023-02-11 18:03:26 -0800

    working on a new Sequelize validation error message for Products

--- models/index.js
+++ models/index.js
@@ -1,53 +1,46 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
-const Sequelize = require('Sequelize');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+/*const Sequelize = require('Sequelize');*/
 
 // Products belongsTo Category
-/*const Category = sequelize.define('Category', {});*/
-
-
-  Category.hasMany(Product, {
-    foreignKey: 'category_id',
-  });
+Product.belongsTo(Category, {
+  foreginKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Categories have many Products
 /*const Product = sequelize.define('Product', {});*/
 
-  Product.hasMany(Category, {
-    foreignKey: 'category_id',
-  });
-
-
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
 /*const ProductTag = sequelize.define('ProductTag', {});*/
 
-  ProductTag.hasMany(Product, {
-    foreignKey: 'product_id',
-  });
-  ProductTag.hasMany(Tag, {
-    foreignKey: 'tag_id',
-  });
-
+Product.belongsToMany(Tag, {
+  foreignKey: "product_id",
+  through: ProductTag,
+});
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
 /*const Tag = sequelize.define('Tag', {});*/
 
-  Tag.belongsToMany(Product, {
-    through: ProductTag,
-    foreignKey: 'tag_id',
-  });
-
-
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
   Category,
   Tag,
   ProductTag,
- }
-
+};
